0c73abb2a98141adc6030961d814c82378e9f293
Kevin Hong <kevin@dnastack.com>
2024-09-19 13:45:42 -0400

    [CU-86b2732g4] update google provider versions

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.3.9"
+  required_version = "~> 1.7"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.55.0"
+      version = "~> 6.2.0"
     }
     random = {
       source  = "hashicorp/random"
