ff952869cdef50f0eb6a7daf88d59a4eed3495f5
Kevin Hong <kevin@dnastack.com>
2024-09-26 16:38:51 -0400

    [CU-86b20ppjf] add index to project resource reference

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_project" "project" {
 data "google_project" "project" {
   project_id = var.deployment_project_id
 
-  depends_on = [google_project.project]
+  depends_on = [google_project.project[0]]
 }
 
 data "google_client_openid_userinfo" "provider_credentials" {}
